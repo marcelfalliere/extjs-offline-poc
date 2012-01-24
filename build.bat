400bd52bd93a4cd6b97a40559306f106b9e27f4b
frederic.falliere <frederic.falliere@gmail.com>
2012-01-24 16:02:58 +0100

    correction du build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 CALL sencha create jsb  -a http://localhost:3000/index.html -p public/app.jsb3
 CALL sencha build -p public/app.jsb3 -d .
-MOVE all-classes.js /public
-MOVE app-all.js /public
+MOVE all-classes.js "D:\EasyPHP5.2.10\www\extjs-offline-poc\public"
+MOVE app-all.js "D:\EasyPHP5.2.10\www\extjs-offline-poc\public"
 CALL git add .
 CALL git commit -a -m %1
