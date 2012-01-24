a6ca32a3863de03c10d3cce30d775a6cc296b4e0
frederic.falliere <frederic.falliere@gmail.com>
2012-01-24 16:01:31 +0100

    correction du build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 CALL sencha create jsb  -a http://localhost:3000/index.html -p public/app.jsb3
 CALL sencha build -p public/app.jsb3 -d .
+MOVE all-classes.js /public
+MOVE app-all.js /public
 CALL git add .
 CALL git commit -a -m %1
-CALL git push heroku master
